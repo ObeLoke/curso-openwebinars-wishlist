d72b7a81a4cade8f2500ca91906f534a6e61a537
Miguel Veloso <obeloke@hotmail.com>
2021-05-28 23:55:58 -0400

    Segunda parte del ejercicio lista

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,24 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import WishInput from './WishInput';
 import WishList from './WishList/WishList';
 
-const wishes = [
+const initialWishes = [
   { done: true, text: 'Comprar Pan' },
   { done: false, text: 'Aprobar el curso' },
   { done: false, text: 'Dormir bien' },
   { done: true, text: 'Tomar agua' },
 ];
 
-const App = () => (
-  <div className="app">
-    <h1>My wishlist app</h1>
-    <WishInput />
-    <WishList wishes={wishes} />
-    <button className="wish-clear" type="button">
-      Archive done
-    </button>
-  </div>
-);
+const App = () => {
+  const [wishes, setWishes] = useState(initialWishes);
+  return (
+    <div className="app">
+      <h1>My wishlist app</h1>
+      <WishInput
+        onNewWish={(wish) => {
+          setWishes([wish, ...wishes]);
+        }}
+      />
+      <WishList wishes={wishes} />
+      <button className="wish-clear" type="button">
+        Archive done
+      </button>
+    </div>
+  );
+};
 
 export default App;
